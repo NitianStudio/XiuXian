27edbeaf26ef77d008c1646c8063c22af6da6f0b
baka4n <qq474899581@gmail.com>
2024-10-28 16:34:37 +0800

    更新

---
+++ src/main/java/io/github/nitianstudio/xiuxian/api/practice/BreathLevel.java
@@ -0,0 +1,4 @@
+package io.github.nitianstudio.xiuxian.api.practice;
+
+public enum BreathLevel {//练气等级
+}
---
+++ src/main/java/io/github/nitianstudio/xiuxian/api/practice/FleshLevel.java
@@ -0,0 +1,4 @@
+package io.github.nitianstudio.xiuxian.api.practice;
+
+public class FleshLevel {//炼体
+}
